8dd2e8c43a8738a5841caaee784e05c7877066a5
Stacey Astoveza <staceyastoveza@transfix.io>
2017-08-01 19:05:22 -0400

    LIDS

--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,14 @@
 require_relative '../config/environment'
 
+# cli = CommandLineInterface.new
+
+# cli.greet
+# cli.get_voter_name
 greet
-get_voter_name
+loop do
+    found = get_voter_name
+    if found == true
+    	break
+	end
+end
+# menu_option
